0f7f98967ece1609f10d7672742002d9ac7a0998
thomasrosen <thomas.rosen@me.com>
2024-12-14 01:59:04 +0100

    fix button in button error

--- src/components/AdminMenu.tsx
+++ src/components/AdminMenu.tsx
@@ -12,7 +12,7 @@ import { Button } from "./ui/button";
 export function AdminMenu() {
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
+      <DropdownMenuTrigger asChild>
         <Button variant="ghost">Menu</Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="start">
