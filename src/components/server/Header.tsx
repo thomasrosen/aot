f94abe7d07d58f0c294d7f872f9f8e56797404e9
thomasrosen <thomas.rosen@me.com>
2024-12-29 03:01:22 +0100

    website icon

--- src/components/server/Header.tsx
+++ src/components/server/Header.tsx
@@ -9,6 +9,7 @@ import { loadTranslations } from "@/lib/server/fluent-server";
 import { getLocale } from "@/lib/server/getLocale";
 import { userHasOneOfPermissions } from "@/lib/server/permissions";
 import Link from "next/link";
+import { Icon } from "../Icon";
 
 export async function Header() {
   const session = await auth();
@@ -24,7 +25,8 @@ export async function Header() {
   return (
     <header className="sticky top-0 bg-background text-foreground border-b z-20">
       <div className="w-content max-w-full mx-auto px-8 flex gap-4 items-center justify-between h-16">
-        <Link href="/" className="shrink-0">
+        <Link href="/" className="shrink-0 inline-flex items-center gap-2">
+          <Icon name="inventory_2" />
           <H1 className="text-lg lg:text-2xl">{t("app-title")}</H1>
         </Link>
         <nav className="w-full">
